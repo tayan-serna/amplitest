54011925b54817eb3dd8f0e3b7f09bb76de3cdb9
tayan-serna <adrian_serna_92@hotmail.com>
2020-09-10 18:46:43 -0500

    Dev version

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          HOLA MUNDO!
+          HOLA MUNDO EN DEV!
         </p>
         <a
           className="App-link"
